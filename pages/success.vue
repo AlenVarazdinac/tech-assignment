9ce922aa438f249d8c21dc53eacb1769923c5d4e
Alen Varazdinac <alen.varazdinac@gmail.com>
2026-05-16 19:59:16 +0200

    Remove ClientOnly from success page

--- pages/success.vue
+++ pages/success.vue
@@ -11,34 +11,32 @@ const receiveUpdates = computed(() => user.value?.receiveUpdates ?? false)
 
 <template>
   <div class="page n:flex n:items-center n:justify-center n:min-h-screen">
-    <ClientOnly>
-      <div class="n:container-xs">
-        <nord-card padding="l">
-          <div class="n:flex n:flex-col n:items-center n:gap-l n:text-center">
-            <nord-icon
-              name="interface-checked-circle"
-              size="xl"
-              color="var(--n-color-status-success)"
-            />
+    <div class="n:container-xs">
+      <nord-card padding="l">
+        <div class="n:flex n:flex-col n:items-center n:gap-l n:text-center">
+          <nord-icon
+            name="interface-checked-circle"
+            size="xl"
+            color="var(--n-color-status-success)"
+          />
 
-            <div class="n:flex n:flex-col n:gap-s">
-              <h2 class="n:text-heading-2">
-                You're all set!
-              </h2>
-              <p class="n:text-body">
-                Welcome aboard. We've created your account for
-                <strong>{{ email }}</strong>.
-              </p>
-              <p
-                v-if="receiveUpdates"
-                class="n:text-body-s"
-              >
-                Thanks for subscribing! We'll keep you posted on product updates and announcements.
-              </p>
-            </div>
+          <div class="n:flex n:flex-col n:gap-s">
+            <h2 class="n:text-heading-2">
+              You're all set!
+            </h2>
+            <p class="n:text-body">
+              Welcome aboard. We've created your account for
+              <strong>{{ email }}</strong>.
+            </p>
+            <p
+              v-if="receiveUpdates"
+              class="n:text-body-s"
+            >
+              Thanks for subscribing! We'll keep you posted on product updates and announcements.
+            </p>
           </div>
-        </nord-card>
-      </div>
-    </ClientOnly>
+        </div>
+      </nord-card>
+    </div>
   </div>
 </template>
